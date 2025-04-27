6d8b7550793275eb543f0242eac6182ffff1c49c
caiquemiranda <caiquemirandas@gmail.com>
2025-04-26 23:34:29 -0300

    Atualiza o componente App para melhorar a alternância entre edição e visualização, ajusta estilos CSS dos botões e remove dependências desnecessárias do Tiptap no package.json.

---
+++ model_report/src/components/ReportSheet.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import InsertOptions from './InsertOptions';
+import ReportContent from './ReportContent';
+import SummernoteEditor from '../SummernoteEditor';
+import '../styles/ReportSheet.css';
+
+const ReportSheet = ({
+    isPreview,
+    showOptions,
+    showEditor,
+    contents,
+    handleInsertClick,
+    handleTextClick,
+    handleSaveText,
+    setShowEditor,
+    editorIndex
+}) => {
+    return (
+        <div className="report-sheet-container">
+            <div className="a4-sheet">
+                {!isPreview && (
+                    <button className="insert-topic-btn" onClick={handleInsertClick}>
+                        inserir tópico
+                    </button>
+                )}
+
+                {showOptions && !isPreview && (
+                    <InsertOptions handleTextClick={handleTextClick} />
+                )}
+
+                {showEditor && !isPreview && (
+                    <SummernoteEditor
+                        onSave={handleSaveText}
+                        onClose={() => setShowEditor(false)}
+                    />
+                )}
+
+                <ReportContent
+                    contents={contents}
+                    isPreview={isPreview}
+                    handleTextClick={handleTextClick}
+                />
+            </div>
+        </div>
+    );
+};
+
+export default ReportSheet; 
