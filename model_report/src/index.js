e8f41632d20c914467568978a2ee7c1ea0132b13
caiquemiranda <caiquemirandas@gmail.com>
2025-04-28 22:17:53 -0300

    Implementa lógica de renderização condicional no arquivo index.js para exibir o componente ReportPrintPreview ao acessar a rota '/print-preview', mantendo a renderização do componente App para outras rotas. Essa mudança melhora a usabilidade ao permitir uma visualização específica para impressão.

--- model_report/src/index.js
+++ model_report/src/index.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 import App from './App';
+import ReportPrintPreview from './components/ReportPrintPreview';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const path = window.location.pathname;
+
+if (path === '/print-preview') {
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+      <ReportPrintPreview />
+    </React.StrictMode>
+  );
+} else {
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
