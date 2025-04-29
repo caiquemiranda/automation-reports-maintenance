4742a1313d1800e71d4896fe8d2e211056243caa
caiquemiranda <caiquemirandas@gmail.com>
2025-04-28 23:06:20 -0300

    Atualiza os botões no componente InsertOptions para melhorar a clareza dos textos, alterando as legendas para "Inserir texto", "Inserir tabela" e "Inserir imagem". Essas mudanças visam aprimorar a usabilidade e a compreensão das opções disponíveis.

--- model_report/src/components/InsertOptions.js
+++ model_report/src/components/InsertOptions.js
@@ -4,12 +4,9 @@ import '../styles/InsertOptions.css';
 const InsertOptions = ({ handleTextClick }) => {
     return (
         <div className="insert-options">
-            <button onClick={() => handleTextClick('text')}> Textos </button>
-            <button onClick={() => handleTextClick('table')}> Tabela </button>
-            <button onClick={() => handleTextClick('image-left')}> Foto com legenda lateral esquerda </button>
-            <button onClick={() => handleTextClick('image-right')}> Foto com legenda na lateral direita </button>
-            <button onClick={() => handleTextClick('image-below')}> Foto com legenda na lateral abaixo </button>
-            <button onClick={() => handleTextClick('image-above')}> Foto com legenda acima </button>
+            <button onClick={() => handleTextClick('text')}>Inserir texto</button>
+            <button onClick={() => handleTextClick('table')}>Inserir tabela</button>
+            <button onClick={() => handleTextClick('image')}>Inserir imagem</button>
         </div>
     );
 };
