83190eaae843eb36bc67c096cf82b12ba81fd9f3
caiquemiranda <caiquemirandas@gmail.com>
2025-04-29 23:24:06 -0300

    Adiciona o componente Sidebar e ajusta o layout da aplicação para incluir uma barra lateral, melhorando a organização visual e a usabilidade do conteúdo.

--- ordem-servico-corretiva/os-report-2.0/src/App.js
+++ ordem-servico-corretiva/os-report-2.0/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './components/Header/Header';
+import Sidebar from './components/Sidebar/Sidebar';
 import ServiceOrderInfo from './components/ServiceOrderInfo/ServiceOrderInfo';
 import ServiceDetails from './components/ServiceDetails/ServiceDetails';
 import Conclusion from './components/Conclusion/Conclusion';
@@ -9,13 +10,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Header />
-      <main className="main-content">
-        <ServiceOrderInfo />
-        <ServiceDetails />
-        <Conclusion />
-        <TechnicianInfo />
-      </main>
+      <Sidebar />
+      <div className="content-with-sidebar">
+        <Header />
+        <main className="main-content">
+          <ServiceOrderInfo />
+          <ServiceDetails />
+          <Conclusion />
+          <TechnicianInfo />
+        </main>
+      </div>
     </div>
   );
 }
