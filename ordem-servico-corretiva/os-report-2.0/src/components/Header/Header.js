a73155c5301d9e850449f22b437cbc264775f713
caiquemiranda <caiquemirandas@gmail.com>
2025-04-29 23:22:33 -0300

    Atualiza o componente Header para incluir a logo e ajustar o layout, melhorando a apresentação visual e a usabilidade do cabeçalho.

--- ordem-servico-corretiva/os-report-2.0/src/components/Header/Header.js
+++ ordem-servico-corretiva/os-report-2.0/src/components/Header/Header.js
@@ -2,10 +2,8 @@ import React from 'react';
 import './Header.css';
 
 const Header = () => (
-  <header className="header">
-    <button className="btn">Voltar</button>
-    <span className="title">Documento Finalizado - OS 305489</span>
-    <button className="btn">Imprimir</button>
+  <header className="header header-logo-only">
+    <img src="/logo192.png" alt="Logo IBSistemas" className="logo" />
   </header>
 );
 
