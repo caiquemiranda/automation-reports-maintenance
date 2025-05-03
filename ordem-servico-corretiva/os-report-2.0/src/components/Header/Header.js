b559f978388735a3404a38f5d7ebbbf70d3d6b3d
caiquemiranda <caiquemirandas@gmail.com>
2025-05-02 23:05:51 -0300

    Remove completamente o componente Header, simplificando a estrutura do código.

--- ordem-servico-corretiva/os-report-2.0/src/components/Header/Header.js
+++ ordem-servico-corretiva/os-report-2.0/src/components/Header/Header.js
@@ -1,10 +1,2 @@
-import React from 'react';
-import './Header.css';
-
-const Header = () => (
-  <header className="header header-logo-only">
-    {/* Logo removido */}
-  </header>
-);
-
-export default Header; 
+// Header removido completamente
+export default () => null; 
